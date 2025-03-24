162293d5fae94ee10bcf041541c7dc1a272b3389
derptf2 <derp.gp1@gmail.com>
2025-03-24 18:04:30 +0200

    doesthiswork

---
+++ testcode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//this is just a test file to try and figure out how to commit files on github
+int main()
+{
+    //really just add the fucking commitsz
+ int a,b;
+ cin >>a >>b;
+ // test maybe
+ //just add the fuckass commit bro
+}
