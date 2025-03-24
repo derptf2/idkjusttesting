ccaa5b6a7f966602bde5a30397f445c4b3f24157
derptf2 <derp.gp1@gmail.com>
2025-03-24 18:00:31 +0200

    Update testcode.cpp

--- testcode.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-//this is just a test file to try and figure out how to commit files on github
-int main()
-{
- int a,b;
- cin >>a >>b;
- cout >> a >>b;
-}
