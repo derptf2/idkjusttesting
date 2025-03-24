f4cade55f287a92906c5351f3d76ee004e6e162f
derptf2 <derp.gp1@gmail.com>
2025-03-24 18:01:15 +0200

    please bro

---
+++ testcode.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+//this is just a test file to try and figure out how to commit files on github
+int main()
+{
+ int a,b;
+ cin >>a >>b;
+ cout >> a >>b;
+}
